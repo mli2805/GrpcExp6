4e62815664f3c29a4800a050297b74f756e735c0
mli2805 <mli2805@gmail.com>
2022-01-02 21:30:49 +0300

    my message with timestamp, list and byte array

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,11 +1,56 @@
-﻿using Grpc.Net.Client;
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
 using GrpcService;
 
 // The port number must match the port of the gRPC server.
-using var channel = GrpcChannel.ForAddress("https://localhost:7142");
+// using var channel = GrpcChannel.ForAddress("https://localhost:7142");
+using var channel = GrpcChannel.ForAddress("https://localhost:5001");
 var client = new Greeter.GreeterClient(channel);
-var reply = await client.SayHelloAsync(
-    new HelloRequest { Name = "Вася" });
-Console.WriteLine("Greeting: " + reply.Message);
-Console.WriteLine("Press any key to exit...");
+
+var priceRequest = CreateRequest();
+var priceResponse = await client.SayPriceAsync(priceRequest);
+if (priceResponse == null)
+{
+    Console.WriteLine("Failed to get price");
+    return;
+}
+
+if (priceResponse.RequestId != priceRequest.RequestId)
+{
+    Console.WriteLine("Wrong Request ID!");
+    return;
+}
+
+Console.WriteLine("Price response is: ");
+Console.WriteLine($" requestId: {priceResponse.RequestId}");
+Console.WriteLine($" userId: {priceResponse.UserId}");
+Console.WriteLine($" isSuccess: {priceResponse.IsSuccess}");
+Console.WriteLine($" errorMessage: {priceResponse.ErrorMessage}");
+Console.WriteLine($" price: {priceResponse.Price}");
+Console.Write(" sorBytes: ");
+var bytes = priceResponse.SorBytes.ToByteArray();
+foreach (var b in bytes)
+{
+    Console.Write($"{b} ");
+}
+Console.WriteLine("\nPress any key to exit...");
 Console.ReadKey();
+
+
+PriceRequest CreateRequest()
+{
+    var customer = new Customer() { Id = "VasyaId", Name = "Vasya" };
+    for (int i = 0; i < 10; i++)
+    {
+        customer.Orders.Add(
+            new Order
+            {
+                Id = i,
+                Start = Timestamp.FromDateTime(DateTime.Today.AddDays(10 - i).ToUniversalTime()),
+                Goods = { "Book" + i, "Pen" + i, "Bicycle" + 1 }
+            });
+    }
+
+    return new PriceRequest()
+        { RequestId = Guid.NewGuid().ToString(), UserId = "VasyaId", Customer = customer };
+}
--- GrpcService/Services/GreeterService.cs
+++ GrpcService/Services/GreeterService.cs
@@ -1,3 +1,4 @@
+using Google.Protobuf;
 using Grpc.Core;
 
 namespace GrpcService.Services
@@ -17,5 +18,21 @@ namespace GrpcService.Services
                 Message = "Hello, " + request.Name
             });
         }
+
+        public override Task<PriceResponse> SayPrice(PriceRequest request, ServerCallContext context)
+        {
+            var bytes = new byte[] { 12, 34, 56, 78, 100, 122};
+            var priceResponse = new PriceResponse()
+            {
+                RequestId = request.RequestId, 
+                UserId = request.UserId, 
+                IsSuccess = true, 
+                Price = 12345.6789012345,
+                SorBytes = ByteString.CopyFrom(bytes),
+            };
+            return Task.FromResult(priceResponse);
+        }
+
+        
     }
 }
