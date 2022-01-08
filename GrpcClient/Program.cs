3e53fec9cc2455eba30b001da7112bf87db2ea0a
mli2805 <mli2805@gmail.com>
2022-01-08 09:03:15 +0300

    ports are fixed, added Program and Main

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -2,82 +2,88 @@
 using Grpc.Net.Client;
 using GrpcService;
 
-// The port number must match the port of the gRPC server.
-// using var channel = GrpcChannel.ForAddress("https://localhost:7142");
-using var channel = GrpcChannel.ForAddress("https://localhost:5001");
-var client = new Greeter.GreeterClient(channel);
+namespace GrpcClient;
 
-var priceRequest = CreateRequest();
-PriceResponse priceResponse;
-try
+class Program
 {
-    priceResponse = await client.SayPriceAsync(priceRequest);
-}
-catch (Exception e)
-{
-    Console.WriteLine(e);
-    return;
-}
-
-if (priceResponse == null)
-{
-    Console.WriteLine("Failed to get price");
-    return;
-}
-
-if (priceResponse.RequestId != priceRequest.RequestId)
-{
-    Console.WriteLine("Wrong Request ID!");
-    return;
-}
-
-Console.WriteLine("Price response is: ");
-Console.WriteLine($" requestId: {priceResponse.RequestId}");
-Console.WriteLine($" userId: {priceResponse.UserId}");
-Console.WriteLine($" isSuccess: {priceResponse.IsSuccess}");
-Console.WriteLine($" errorMessage: {priceResponse.ErrorMessage}");
-Console.WriteLine($" price: {priceResponse.Price}");
-Console.Write(@" sorBytes: c:\temp\sor\grpc.sor");
-var bytes = priceResponse.SorBytes.ToByteArray();
-byte[] fileBytes = new byte[priceResponse.FileLen];
-Array.ConstrainedCopy(bytes, priceResponse.FileLen * 9, fileBytes, 0, priceResponse.FileLen);
-
-File.WriteAllBytes(@"c:\temp\sor\grpc9.sor", fileBytes);
-Console.WriteLine("\n\nPress any key to use second service...\n\n");
-Console.ReadKey();
-
-var secondClient = new Seconder.SeconderClient(channel);
-await RegisterUser(secondClient, "PetyaId");
-await RegisterUser(secondClient, "VasyaId");
-await RegisterUser(secondClient, "SeriyId");
-
-
-Console.WriteLine("\nPress any key to exit...");
-Console.ReadKey();
-
-
-PriceRequest CreateRequest()
-{
-    var customer = new Customer() { Id = "VasyaId", Name = "Vasya" };
-    for (int i = 0; i < 10; i++)
+    static async Task Main()
     {
-        customer.Orders.Add(
-            new Order
-            {
-                Id = i,
-                Start = Timestamp.FromDateTime(DateTime.Today.AddDays(10 - i).ToUniversalTime()),
-                Goods = { "Book" + i, "Pen" + i, "Bicycle" + 1 }
-            });
+
+        // The port number must match the port of the gRPC server.
+        // using var channel = GrpcChannel.ForAddress("https://localhost:7142");
+        using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+        var client = new Greeter.GreeterClient(channel);
+
+        var priceRequest = CreateRequest();
+        PriceResponse priceResponse;
+        try
+        {
+            priceResponse = await client.SayPriceAsync(priceRequest);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return;
+        }
+
+        if (priceResponse == null)
+        {
+            Console.WriteLine("Failed to get price");
+            return;
+        }
+
+        if (priceResponse.RequestId != priceRequest.RequestId)
+        {
+            Console.WriteLine("Wrong Request ID!");
+            return;
+        }
+
+        Console.WriteLine("Price response is: ");
+        Console.WriteLine($" requestId: {priceResponse.RequestId}");
+        Console.WriteLine($" userId: {priceResponse.UserId}");
+        Console.WriteLine($" isSuccess: {priceResponse.IsSuccess}");
+        Console.WriteLine($" errorMessage: {priceResponse.ErrorMessage}");
+        Console.WriteLine($" price: {priceResponse.Price}");
+        Console.Write(@" sorBytes: c:\temp\sor\grpc.sor");
+        var bytes = priceResponse.SorBytes.ToByteArray();
+        byte[] fileBytes = new byte[priceResponse.FileLen];
+        Array.ConstrainedCopy(bytes, priceResponse.FileLen * 9, fileBytes, 0, priceResponse.FileLen);
+
+        File.WriteAllBytes(@"c:\temp\sor\grpc9.sor", fileBytes);
+        Console.WriteLine("\n\nPress any key to use second service...\n\n");
+        Console.ReadKey();
+
+        var secondClient = new Seconder.SeconderClient(channel);
+        await RegisterUser(secondClient, "PetyaId");
+        await RegisterUser(secondClient, "VasyaId");
+        await RegisterUser(secondClient, "SeriyId");
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
     }
 
-    return new PriceRequest()
-    { RequestId = Guid.NewGuid().ToString(), UserId = "VasyaId", Customer = customer };
-}
+    static PriceRequest CreateRequest()
+    {
+        var customer = new Customer() { Id = "VasyaId", Name = "Vasya" };
+        for (int i = 0; i < 10; i++)
+        {
+            customer.Orders.Add(
+                new Order
+                {
+                    Id = i,
+                    Start = Timestamp.FromDateTime(DateTime.Today.AddDays(10 - i).ToUniversalTime()),
+                    Goods = { "Book" + i, "Pen" + i, "Bicycle" + 1 }
+                });
+        }
 
-async Task RegisterUser(Seconder.SeconderClient seconderClient, string s)
-{
-    var response = await seconderClient.RegisterMeAsync(new RegistrationDto()
-        { RequestId = Guid.NewGuid().ToString(), UserId = s });
-    Console.WriteLine(response.IsSuccess ? $"{s} registered successfully!\n\n" : $"Failed to register {s}!\n\n");
-    
+        return new PriceRequest()
+            { RequestId = Guid.NewGuid().ToString(), UserId = "VasyaId", Customer = customer };
+    }
+
+    static async Task RegisterUser(Seconder.SeconderClient seconderClient, string s)
+    {
+        var response = await seconderClient.RegisterMeAsync(new RegistrationDto()
+            { RequestId = Guid.NewGuid().ToString(), UserId = s });
+        Console.WriteLine(response.IsSuccess ? $"{s} registered successfully!\n\n" : $"Failed to register {s}!\n\n");
+    }
 }
--- GrpcService/Program.cs
+++ GrpcService/Program.cs
@@ -1,36 +1,29 @@
 using GrpcService.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace GrpcService;
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-
-// Add services to the container.
-builder.Services.AddGrpc();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-// app.MapGrpcService<GreeterService>().RequireHost("*:5001");
-// app.MapGrpcService<SecondService>().RequireHost("*:5001");
-
-app.UseRouting();
-app.MapWhen(context => context.Connection.LocalPort == 1000, newApp => {
-    newApp.UseRouting();
-    newApp.UseEndpoints(endpoints =>
+class Program
+{
+    static void Main(string[] args)
     {
-        endpoints.MapGrpcService<GreeterService>();
-    });
-});
-app.MapWhen(context => context.Connection.LocalPort == 2000, newApp => {
-    newApp.UseRouting();
-    newApp.UseEndpoints(endpoints =>
-    {
-        endpoints.MapGrpcService<SecondService>();
-    });
-});
 
+        var builder = WebApplication.CreateBuilder(args);
 
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+        // Additional configuration is required to successfully run gRPC on macOS.
+        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
-app.Run();
+        // Add services to the container.
+        builder.Services.AddGrpc();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        app.MapGrpcService<GreeterService>();
+        app.MapGrpcService<SecondService>();
+
+        app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+        app.Run();
+
+    }
+}
