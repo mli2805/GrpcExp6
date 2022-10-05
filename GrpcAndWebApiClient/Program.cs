991e60ee94523c7f19a842266ce3f77ac01b5349
mli2805 <mli2805@gmail.com>
2022-10-05 12:02:23 +0300

    new service-client pair

---
+++ GrpcAndWebApiClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Net.Client;
+using GrpcAndWebApiService;
+
+namespace GrpcAndWebApiClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var serverAddress = "localhost";
+            using var channel1 = GrpcChannel.ForAddress($"http://{serverAddress}:11740");
+            var client1 = new Greeter.GreeterClient(channel1);
+            var response = await client1.SayHelloAsync(new HelloRequest() { Name = "Leanid" });
+            if (response == null)
+            {
+                Console.WriteLine("response is null");
+            }
+            else
+            {
+                Console.WriteLine($"response is {response.Message}");
+            }
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
